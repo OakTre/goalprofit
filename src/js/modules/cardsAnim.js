1821c7d7078616a563161c9eabfd680b2dc16125
Nail <nailgalimov8@gmail.com>
2022-02-14 14:10:07 +0300

    upd

--- src/js/modules/cardsAnim.js
+++ src/js/modules/cardsAnim.js
@@ -1,39 +1,122 @@
-import gsap from "gsap";
+import gsap, { to } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {TweenMax, TimelineMax} from 'gsap';
+import { TimelineLite } from "gsap/gsap-core";
 
 export default () => {
 	gsap.registerPlugin(ScrollTrigger);
 
 	const playAnim = () => {
-		const timeline = gsap.timeline();
+		const timeline = new TimelineLite();
 
 		gsap.set(".steps__rotated-block", {opacity: 0});
-
-		ScrollTrigger.create({
-			animation: timeline,
-			trigger: ".steps__blocks",
-			start: "+100px 20%",
-			end: "+=" + (window.innerHeight * 2),
-			scrub: true,
-			pin: true,
-			// pinSpacing: false,
-		})
+		gsap.set(".js-text-block2", {opacity: 0});
+		gsap.utils.toArray(".steps__rotated-block-item").forEach((item)=>{
+			gsap.set(item, {opacity: 0})
+		});
+		gsap.utils.toArray(".steps__card").forEach((item)=>{
+			gsap.set(item, {opacity:0});
+			gsap.set(item.querySelector(".steps__card-content-wrapper"), {
+				opacity: 0
+			})
+		});
+		// ScrollTrigger.create({
+		// 	animation: timeline,
+		// })
 
 		timeline
 			.to(".steps__blocks", {
+				scrollTrigger: {
+					trigger: ".steps__blocks",
+					start: "+50px 30%",
+					// end: "+=" + (window.innerHeight * 2),
+					scrub: true,
+					pin: true,
+					pinSpacing: false,
+				},
 				rotation: 45,
 				x: "-63rem",
-				y: "67rem",
 				width: "42.2rem",
 				height: "42.2rem",
-				autoAlpha: 0
-			})
-			.to(".steps__rotated-block", {
-				opacity: 1
-			})
-			.from(".steps__rotated-block-item", {
 				opacity: 0
 			})
+			.to(".js-text-block2", {
+				scrollTrigger: {
+					trigger: ".js-text-block2",
+					start: "+2rem 30%",
+					// end: "+=" + (window.innerHeight * 2),
+					scrub: true,
+					// pin: true,
+					pinSpacing: false,
+				},
+				opacity: 1
+			})
+			.to(".steps__rotated-block", {
+				scrollTrigger: {
+					trigger: ".steps__rotated-block",
+					start: "+100px 11%",
+					// end: "+=" + (window.innerHeight * 2),
+					scrub: true,
+					pin: ".js-rotated-block",
+					// pinSpacing: false,
+				},
+				opacity: 1
+			})
+			.to(".steps__rotated-block-item", {
+				scrollTrigger: {
+					trigger: ".steps__rotated-block-item",
+					start: "+5rem 11%",
+					// end: "+=" + (window.innerHeight * 2),
+					scrub: true,
+					// pin: ".js-rotated-block",
+					// pinSpacing: false,
+				},
+				opacity: 1
+			})
+
+			const tml2 = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".steps__card:nth-child(1)",
+					// scrub: true,
+				}
+			});
+
+			tml2.to(".steps__card:nth-child(1)", {
+				opacity: 1,
+				duration: 1,
+				delay: 0.6,
+			});
+
+			gsap.utils.toArray(".steps__card:not(:first-child)").forEach((item)=>{
+				const tml3 = gsap.timeline({
+					scrollTrigger: {
+						trigger: item,
+						start: "top 50%"
+					}
+				});
+
+				tml3.to(item, {
+					opacity: 1,
+					duration: 1,
+					delay: 0.6,
+				});
+			});
+
+
+			// gsap.utils.toArray(".steps__rotated-block-item").forEach((item)=>{
+			// 	timeline.to(item, {
+			// 		scrollTrigger: {
+			// 			animation: timeline,
+			// 			trigger: item,
+			// 			start: "+5rem 11%",
+			// 			// end: "+=" + (window.innerHeight * 2),
+			// 			scrub: true,
+			// 			pin: ".js-rotated-block",
+			// 			// pinSpacing: false,
+			// 		},
+			// 		opacity: 1
+			// 	})
+			// });
 	};
 
 
