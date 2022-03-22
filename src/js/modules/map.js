96b11b6bfb4f1f594a6347caa25a4c1260ffbade
OakTre <nailgalimov8@gmail.com>
2022-03-22 11:37:41 +0300

    upd

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,20 +1,67 @@
 export default () => {
-	// function loadScript() {
-	// 	var script = document.createElement('script');
-	// 	script.type = 'text/javascript';
-	// 	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBM-QwzS2G0X_PiDPqQX9v4p1a66oHG2cE&' + 'callback=initialize';
-	// 	document.body.appendChild(script);
-	// }
+	if (!document.querySelector(".js-map-item")) return;
 
-	if (!document.querySelector("input[name=coordinates]")) return;
+	const mapitems = Array.from(document.querySelectorAll(".js-map-item"));
 
-	let inptCoords = document.querySelector("input[name=coordinates]").value;
-	inptCoords = JSON.parse(inptCoords);
-	let lat;
-	let lng;
-	let latlngStr;
-	let coordinates;
 	let markerImage = document.querySelector("#contact-us-map").getAttribute("data-map-icon");
+
+	let map = new google.maps.Map(document.getElementById("contact-us-map"), {
+		center: new google.maps.LatLng(0, 0),
+		zoom: 3
+	});
+
+	let infowindow = new google.maps.InfoWindow();
+	let bounds = new google.maps.LatLngBounds();
+
+	mapitems.forEach((item, index)=>{
+		let inptCoords = item.querySelector("input[name=coords]").value;
+		inptCoords = JSON.parse(inptCoords);
+		console.log(inptCoords);
+		let lat = inptCoords[0];
+		let lng = inptCoords[1];
+		let coordinates = {
+			lat: lat,
+			lng: lng
+		};
+
+		let marker = new google.maps.Marker({
+			position: coordinates,
+			map: map,
+			icon: markerImage
+		});
+
+		console.log(item.dataset.name);
+
+		bounds.extend(marker.position);
+
+		let layout = `
+            <div class="mark">
+                <span class="mark__heading">
+                    ${item.dataset.name}
+                    <span>${item.dataset.location}</span>
+                </span>
+                <p class="mark__coordinates">${item.dataset.street}</p>
+                <span class="mark__legend">Email</span>
+                <a class="mark__link" href="mailto:${item.dataset.emal}">${item.dataset.emal}</a>
+                <span class="mark__legend">Phone Number</span>
+                <a class="mark__link" href="tel:+05251565481258">${item.dataset.tel}</a>
+            </div>
+        `;
+
+		google.maps.event.addListener(marker, 'click', (function (marker, index) {
+			return function () {
+				infowindow.setContent(layout);
+				infowindow.open(map, marker);
+			}
+		})(marker, index));
+	});
+
+	map.fitBounds(bounds);
+
+	var listener = google.maps.event.addListener(map, "idle", function () {
+		map.setZoom(3);
+		google.maps.event.removeListener(listener);
+	});
 
 	let styles = [{
 			"stylers": [{
@@ -39,59 +86,6 @@ export default () => {
 			}]
 		}
 	]
-
-	let map = new google.maps.Map(document.getElementById("contact-us-map"), {
-		center: new google.maps.LatLng(0, 0),
-		zoom: 3
-	});
-
-	let infowindow = new google.maps.InfoWindow();
-	let bounds = new google.maps.LatLngBounds();
-
-	for (let i = 0; i < inptCoords.length; i++) {
-		let lat = inptCoords[i].coords[0];
-		let lng = inptCoords[i].coords[1];
-		let coordinates = {
-			lat: lat,
-			lng: lng
-		};
-
-		let marker = new google.maps.Marker({
-			position: coordinates,
-			map: map,
-			icon: markerImage
-		});
-
-		bounds.extend(marker.position);
-
-		let layout = `
-            <div class="mark">
-                <span class="mark__heading">
-                    ${inptCoords[i].name}
-                    <span>${inptCoords[i].country}</span>
-                </span>
-                <p class="mark__coordinates">${inptCoords[i].street}</p>
-                <span class="mark__legend">Email</span>
-                <a class="mark__link" href="mailto:${inptCoords[i].email}">${inptCoords[i].email}</a>
-                <span class="mark__legend">Phone Number</span>
-                <a class="mark__link" href="tel:+05251565481258">${inptCoords[i].tel}</a>
-            </div>
-        `;
-
-		google.maps.event.addListener(marker, 'click', (function (marker, i) {
-			return function () {
-				infowindow.setContent(layout);
-				infowindow.open(map, marker);
-			}
-		})(marker, i));
-	}
-
-	map.fitBounds(bounds);
-
-	var listener = google.maps.event.addListener(map, "idle", function () {
-		map.setZoom(3);
-		google.maps.event.removeListener(listener);
-	});
 
 	map.setOptions({
 		styles: styles
